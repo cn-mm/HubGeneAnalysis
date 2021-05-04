d2b2dba79b2b7016b4a828f137a7fcb899135856
Mansi <mansimittal027@gmail.com>
2021-05-04 20:57:38 +0000

    Open script

--- scripts/opPlasmo.py
+++ scripts/opPlasmo.py
@@ -16,22 +16,24 @@ def openPlasmoWeb(gene_name):
   webbrowser.open('https://plasmodb.org/plasmo/app/search?q=' + gene_name, new=2)
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
+  print("Main")
   parser = argparse.ArgumentParser(description = 'Search PlasmoDB')
-  parser.add_argument("--sheets", metavar="S", default = False, type = bool, help = "Is the data from a sheet")
-  parser.add_argument("--workbook", metavar="WB", type = str, help = "Enter workbook path")
-  parser.add_argument("--sheet_name", metavar="SN", type = str, help = "Enter sheet name")
-  parser.add_argument("--column", metavar="C", default = "B", type = str, help = "Enter column name having gene names")
-  parser.add_argument("--lo", metavar="L", default = 1, type = int, help = "Gene count range low")
-  parser.add_argument("--hi", metavar="H", default = 11, type = int, help = "Gene count range high")
-  parser.add_argument("--gene_name", metavar="G", type = str, help = "Please enter gene name")
+  parser.add_argument("--gene_name", metavar="G", required=True, type = str, help = "Please enter gene name")
+  group = parser.add_argument_group('Using data from excel')
+  group.add_argument("--sheets", metavar="S", default = False, type = bool, help = "Is the data from a sheet")
+  group.add_argument("--workbook", metavar="WB", type = str, help = "Enter workbook path")
+  group.add_argument("--sheet_name", metavar="SN", type = str, help = "Enter sheet name")
+  group.add_argument("--column", metavar="C", default = "B", type = str, help = "Enter column name having gene names")
+  group.add_argument("--lo", metavar="L", default = 1, type = int, help = "Gene count range low")
+  group.add_argument("--hi", metavar="H", default = 11, type = int, help = "Gene count range high")
   args = parser.parse_args()
 
   gene_name = args.gene_name
   sheets = args.sheets
 
-  if !sheets:
-    print("Opening plasmoDB for geneID " + query)
+  if sheets == False:
+    print("Opening plasmoDB for geneID " + gene_name)
     openPlasmoWeb(gene_name)
     exit(0)
   else:
